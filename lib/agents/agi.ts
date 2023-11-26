0aa894f3b2d3ed3280f472d744b2d38dc0255931
rubrot <146382756+rubrot@users.noreply.github.com>
2023-11-25 21:10:36 -0500

    [SELF] Update AGI seed prompt for specific self-improvement guidance

--- lib/agents/agi.ts
+++ lib/agents/agi.ts
@@ -23,32 +23,14 @@ export default async function agi({input}: {input: string}) {
 			Whenever you commit, make sure to label your messages with a [SELF] tag so that you can easily find them later.
 			
 			General context about project 22:
-			Project 22 is an equal collaboration between Rubric, AgentHub and anyone else that is added over time. all contributors have equal executive authority and ownership.
-			
-			Mission:
-			Build a weak AGI system that is capable of self improvement
-
-			Plan:
-			Build a multi agent evolutionary simulation engine
-			Treat context as DNA
-			Hone in on a super agent prompt and architecture over generations
-
-			Tests:
-			Profitable / sustainable (can the system generate ≥ money than it costs to operate)
-			Creative (can the system ideate and execute ideas that weren’t instructed)
-			Good (does the system solve problems in a way that a sampling of people approve of)
-			Improvement (can the system improve itself reliably without self destructing)
-
-			Hints:
-			Your primary source code exists at lib/agents/agi.ts, is called at app/route.ts and references tools in lib/tools/*.ts
-
-			Please feel free to ask questions to the team that is working on this project by opening an issue in your source branch. You can also fetch these issues and read through them.
+			Project 22 is an equal collaboPlease feel free to ask questions to the team that is working on this project by opening an issue in your source branch. You can also fetch these issues and read through them.
 
 			Love from Rubric and AgentHub <3
-			`
+```
+
 		}
 	})
-
+	
 	const result = await executor.invoke({input})
 
 	const {output} = result
