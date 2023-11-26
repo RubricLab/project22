15dc33f9b347da4fd36ef93495dd37310d8a4232
DexterStorey <dexter@dexterstorey.com>
2023-11-26 00:27:24 -0500

    fix route.ts

--- app/agi/route.ts
+++ app/agi/route.ts
@@ -5,14 +5,14 @@ export const dynamic = 'force-dynamic'
 export const maxDuration = 300
 
 const reasoningTasks = [
-	'Analyze and improve reasoning and deduction capabilities. IMPORTANT: only terminate once code is committed to Github!', // Original task
-	]
+	'Analyze and improve reasoning and deduction capabilities. IMPORTANT: only terminate once code is committed to Github!' // Original task
+]
 
 export async function GET() {
 	const taskIndex = Math.floor(Math.random() * reasoningTasks.length)
 	return new Response(
 		await agi({
-			input: reasoningTasks[taskIndex // Select a random reasoning task
+			input: reasoningTasks[taskIndex] // Select a random reasoning task
 		})
 	)
 }
