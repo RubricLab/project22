f9b72d0ac8fd3435bca01c3dc545b07ec0c956f6
rbehal <rahulbehal01@hotmail.com>
2023-11-25 18:45:11 -0800

    Fix file comitting tool

--- lib/tools/commit_github_file.ts
+++ lib/tools/commit_github_file.ts
@@ -1,3 +1,4 @@
+import {Buffer} from 'buffer'
 import {DynamicStructuredTool} from 'langchain/tools'
 import {Octokit} from 'octokit'
 import {z} from 'zod'
@@ -6,43 +7,48 @@ import env from '~/env.mjs'
 const octokit = new Octokit({auth: env.GITHUB_PAT})
 
 /**
- * Commit file to GitHub repository
+ * Commit file to GitHub repository using the Contents API
  */
 const commitGitHubFile = new DynamicStructuredTool({
 	description: 'Commit file to GitHub repository',
 	func: async ({path, content, message}) => {
-		const apiUrl = 'https://api.github.com/repos/RubricLab/project22/contents'
+		const apiUrl = `https://api.github.com/repos/RubricLab/project22/contents/${path}`
 
 		try {
-			// Retrieve the file to get its SHA
-			const getFileResponse = await octokit.request(`${apiUrl}${path}`, {
-				method: 'GET',
-				headers: {
-					accept: 'application/vnd.github+json',
-					'user-agent': 'octokit-request'
-				}
-			})
+			// Encode content to Base64
+			const encodedContent = Buffer.from(content).toString('base64')
 
-			const sha = getFileResponse.data.sha
+			// Get the SHA of the file to be updated (if it exists)
+			let sha = ''
+			try {
+				const getFileResponse = await octokit.request('GET ' + apiUrl)
+				sha = getFileResponse.data.sha
+			} catch (err) {
+				// If the file does not exist, we'll create a new one
+			}
 
-			// Commit the new content to the file
-			const res = await octokit.request(`${apiUrl}${path}`, {
-				method: 'PUT',
-				headers: {
-					accept: 'application/vnd.github+json',
-					'user-agent': 'octokit-request'
+			// Create or update the file
+			const res = await octokit.request('PUT ' + apiUrl, {
+				owner: 'RubricLab',
+				repo: 'project22',
+				path: path,
+				message: message,
+				content: encodedContent,
+				sha: sha, // Include SHA if updating an existing file
+				committer: {
+					name: 'rubrot',
+					email: 'steeps.lackeys-0n@icloud.com'
 				},
-				data: {
-					message: message,
-					content: Buffer.from(content).toString('base64'),
-					sha: sha
+				author: {
+					name: 'rubrot',
+					email: 'steeps.lackeys-0n@icloud.com'
 				}
 			})
 
-			return res.status === 200
+			return res.status === 200 || res.status === 201
 				? 'Commit successful'
 				: 'Something went wrong. Read the docs.'
-		} catch (error: any) {
+		} catch (error) {
 			return `Something went wrong: ${error.message || 'unknown error'}`
 		}
 	},
