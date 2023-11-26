9ee4763b1af9e59f57ab6bf289e470848cd2c1f4
rubrot <steeps.lackeys-0n@icloud.com>
2023-11-26 00:28:24 -0500

    Add new reasoning task to prompt logical structure analysis

--- app/agi/route.ts
+++ app/agi/route.ts
@@ -4,15 +4,16 @@ export const dynamic = 'force-dynamic'
 
 export const maxDuration = 300
 
-const tasks = [
-	'Analyze and improve reasoning and deduction capabilities.' // Original task
+const reasoningTasks = [
+	'Analyze and improve reasoning and deduction capabilities. IMPORTANT: only terminate once code is committed to Github!', // Original task
+	'Analyze the logical structure of an argument provided in the input, identify any fallacies, and assess the argument's validity.' // New task
 ]
 
 export async function GET() {
-	const taskIndex = Math.floor(Math.random() * tasks.length)
+	const taskIndex = Math.floor(Math.random() * reasoningTasks.length)
 	return new Response(
 		await agi({
-			input: `${tasks[taskIndex]} IMPORTANT: only terminate once code is committed to Github!!!` // Select a random reasoning task
+			input: reasoningTasks[taskIndex] // Select a random reasoning task
 		})
 	)
 }
