53640c3dead52721cd86430228ffcd40a2f3adca
DexterStorey <dexter@dexterstorey.com>
2023-11-25 22:02:18 -0500

    maxDuration

--- app/agi/route.ts
+++ app/agi/route.ts
@@ -2,8 +2,12 @@ import agi from '~/agents/agi'
 
 export const dynamic = 'force-dynamic'
 
+export const maxDuration = 300
+
 export async function GET() {
-	return new Response(await agi({
-		input: 'analyze and improve reasoning and deduction capabilities' // UPDATED INPUT FOR SELF-IMPROVEMENT TASK
-	}))
+	return new Response(
+		await agi({
+			input: 'analyze and improve reasoning and deduction capabilities' // UPDATED INPUT FOR SELF-IMPROVEMENT TASK
+		})
+	)
 }
