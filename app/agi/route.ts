f59c7a9198d72f72b4bf8b8494b9fbf96dcdd89f
rubrot <steeps.lackeys-0n@icloud.com>
2023-11-25 23:58:48 -0500

    Update input for specific reasoning task in route.ts

--- app/agi/route.ts
+++ app/agi/route.ts
@@ -7,7 +7,7 @@ export const maxDuration = 300
 export async function GET() {
 	return new Response(
 		await agi({
-			input: 'analyze and improve reasoning and deduction capabilities. IMPORTANT: only terminate once code is committed to Github.' // UPDATED INPUT FOR SELF-IMPROVEMENT TASK
+			input: 'Analyze the logical structure of the following argument and identify any fallacies or weaknesses: "If all mammals are warm-blooded, and all dogs are mammals, then all dogs must be warm-blooded. However, since some warm-blooded creatures are known to fly, all dogs must have the ability to fly."' // UPDATED INPUT FOR SPECIFIC REASONING TASK
 		})
 	)
 }
